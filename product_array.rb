037b686a259919485610410e536e53e01b0cda59
Rio <rnose512@gmail.com>
2017-10-30 08:39:28 -0700

    Greedy solution for product array problem

--- product_array.rb
+++ product_array.rb
@@ -3,6 +3,8 @@
 # that takes an array of integers and returns
 # an array of the products.
 
+# brute force solution:
+
 def get_products_of_all_ints_except_at_index(array)
   result_array = []
   array.each do |num|
@@ -14,3 +16,36 @@ def get_products_of_all_ints_except_at_index(array)
 
   return result_array
 end
+
+# O(n^2) time and O(n) space
+
+# greedy approach
+
+def get_products_of_all_ints_except_at_index(array)
+
+  if array.length < 2
+    raise IndexError, 'You need more than 2 integers to get the products'
+  end
+
+  products_of_integers_except_current_index = []
+
+  product_so_far = 1
+  i = 0
+  while i < array.length
+    products_of_integers_except_current_index[i] = product_so_far
+    product_so_far *= array[i]
+    i += 1
+  end
+
+  product_so_far = 1
+  i = array.length - 1
+  while i >= 0
+    products_of_integers_except_current_index[i] *= product_so_far
+    product_so_far *= array[i]
+    i -= 1
+  end
+
+  return products_of_integers_except_current_index
+end
+
+# O(n) time and O(n) space
